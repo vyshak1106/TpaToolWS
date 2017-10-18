576cc287c14f63151f78afd61d12257dd46b8021
Swapnil Singh <swapnilsingh@Swapnils-MacBook-Pro.local>
2017-10-18 15:32:44 +0530

    Logging implementation

--- src/main/java/com/bmw/tpa/service/impl/TpaServiceImpl.java
+++ src/main/java/com/bmw/tpa/service/impl/TpaServiceImpl.java
@@ -5,6 +5,8 @@ package com.bmw.tpa.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.bmw.tpa.model.Tpa;
 import com.bmw.tpa.service.interfaces.TpaService;
 
@@ -14,11 +16,14 @@ import com.bmw.tpa.service.interfaces.TpaService;
  * @author Swapnil Singh
  */
 public class TpaServiceImpl implements TpaService {
+	
+	private static final Logger LOGGER = Logger.getLogger(TpaServiceImpl.class);
 
 
 	@Override
 	public String getMessage() {
 		// TODO Auto-generated method stub
+		LOGGER.info("Calling Message...");
 		return "Hello World!!";
 	}
 
